94bce97723ad731f7acb16d058f11ac8f25f736e
Alex <alireza.worldkeeper@gmail.com>
2025-04-26 20:50:06 -0400

    Search box linked to Intinerary page to look up location directiry; Background image added; Photo reference added; Search box moved to the center

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,45 @@
 import { useState } from 'react';
 import { Container, Form, Button, InputGroup } from 'react-bootstrap';
+import '../styles/home.css';
+import { useNavigate } from 'react-router-dom';
+
 
 function Home() {
   const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
   const handleSearch = () => {
-    console.log("Searching for:", query); // Add your search logic here
+    navigate('/itinerary_creation', { state: { searchQuery: query } });
   };
 
   return (
-    <Container className="mt-5">
-      <h3 className="text-center mb-2">Plan your perfect road trip across Massachusetts!</h3>
-      <h4 className="text-center mb-2">Explore landmarks, get routes, and discover new places to visit.</h4>
+    <Container className="mt-5 home-background">
+      <h3 className="text-center mb-2 text-white" style={{marginTop: '180px'}}>Plan your perfect road trip across Massachusetts!</h3>
+      <h4 className="text-center mb-2 text-white">Explore landmarks, get routes, and discover new places to visit.</h4>
 
       {/* Search Box Section */}
       <div className="d-flex justify-content-center">
-        <InputGroup style={{ maxWidth: '540px', width: '100%', marginTop: '20px' }}>
+        <InputGroup style={{ maxWidth: '540px', width: '100%', marginTop: '50px' }}>
           <Form.Control
             type="text"
             placeholder="Where is your starting location?"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleSearch();
+              }}}
             style={{ padding: '10px' }}
           />
           <Button variant="primary" onClick={handleSearch}>
             Go
           </Button>
         </InputGroup>
+      </div>
+      <div>
+        <p className="justify-content-center text-center text-white" style={{ marginTop: '20px' }}>
+          Photo by <a href="https://unsplash.com/@geraninmo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Geranimo</a> on <a href="https://unsplash.com/photos/aerial-shot-of-road-surrounded-by-green-trees-qzgN45hseN0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash _</a>
+        </p>
       </div>
     </Container>
   );
