1cd8871aaa2427f092430ca327eed940f5149276
pratheekk524 <36111801+pratheekk524@users.noreply.github.com>
2025-04-29 23:38:52 -0400

    For the love of god please let this be the last commit i will have to make

--- src/components/Itinerary_Creation_Page.jsx
+++ src/components/Itinerary_Creation_Page.jsx
@@ -52,7 +52,7 @@ function Itinerary_Creation_Page() {
 
     const useNearbySearch = async (latitude, longitude) => {
       try {
-        const nearbyResponse = await axios.get('/api/nearby', {
+        const nearbyResponse = await axios.get('/api/location', {
           params: {
             lat: latitude,
             lng: longitude,
@@ -80,7 +80,7 @@ function Itinerary_Creation_Page() {
 
     const fallbackToIP = async () => {
       try {
-        const locationResponse = await axios.get('/api/nearby');
+        const locationResponse = await axios.get('/api/location');
         const { latitude, longitude } = locationResponse.data;
         await useNearbySearch(latitude, longitude);
       } catch (err) {
